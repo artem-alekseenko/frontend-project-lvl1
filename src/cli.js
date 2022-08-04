0a23b25274ea4e26056fe98ff7ebbcf4f0b7647b
a.alekseenko <ivanovo.cwi@gmail.com>
2022-08-04 16:11:00 +0300

    add greetings

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import readlineSync from 'readline-sync';
+
+const findOutUserNameAndGreetings = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hi ' + userName + '!');
+}
+export default  findOutUserNameAndGreetings;
+
